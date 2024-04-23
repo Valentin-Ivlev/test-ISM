04057438610fb899b710f2b47eeb32a02bab521c
Val <val-v-i@yandex.ru>
2024-04-23 23:00:52 +0900

    Calculate month balance fix

--- src/Repository/TransactionRepository.php
+++ src/Repository/TransactionRepository.php
@@ -13,11 +13,9 @@ class TransactionRepository {
 
     public function getMonthlyBalanceByUserId($userId) {
         $sql = "SELECT YEAR(trdate) as year, MONTH(trdate) as month,
-                SUM(CASE WHEN user_accounts.user_id = :userId THEN amount ELSE 0 END) as income,
-                SUM(CASE WHEN user_accounts.user_id = :userId THEN 0 ELSE amount END) as expense
+            SUM(CASE WHEN transactions.account_to IN (SELECT id FROM user_accounts WHERE user_id = :userId) THEN amount ELSE 0 END) as income,
+            SUM(CASE WHEN transactions.account_from IN (SELECT id FROM user_accounts WHERE user_id = :userId) THEN amount ELSE 0 END) as expense
             FROM transactions
-            JOIN user_accounts ON transactions.account_to = user_accounts.id
-            WHERE user_accounts.user_id = :userId
             GROUP BY YEAR(trdate), MONTH(trdate)
             ORDER BY year, month";
         $stmt = $this->db->prepare($sql);
